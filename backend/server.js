b5befa6d6759d17a24b2c18659531071872e46cc
Saptarshi samanta <79819563+yoursaptarshi@users.noreply.github.com>
2023-07-06 01:49:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,7 @@ const { connectDatabase } = require("./config/database");
 const cloudinary = require("cloudinary");
 connectDatabase();
 
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
+
 cloudinary.config({ 
   //enter credentials to test, i have deleted it
 });
